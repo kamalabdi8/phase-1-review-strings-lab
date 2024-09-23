f794479e8f963e451dddc628429e7e7514172c21
kamalabdi8 <kamalabdi042@gmail.com>
2024-09-23 14:20:47 +0300

    strings lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser='Kamal'
+let welcomeMessage="Welcome to Flatbook, Kamal!"
+const excitedWelcomeMessage="WELCOME TO FLATBOOK, KAMAL!"
+const shortGreeting = "Welcome, K!"
+
